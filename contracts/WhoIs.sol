395b26ebb27d4d7604891bdfffc297c5d20fa78e
Max Levitskiy <max@webtree.org>
2022-06-20 09:59:48 +0200

    tst

--- contracts/WhoIs.sol
+++ contracts/WhoIs.sol
@@ -1,19 +1,59 @@
 // SPDX-License-Identifier: MIT
 pragma solidity >=0.4.22 <0.9.0;
-import "truffle/Console.sol";
 
 contract WhoIs {
-    mapping(address => string) public publicKeyToUserId;
-    mapping(string => address) public userIdToPublicKey;
+
+    mapping(address => string) private publicKeyToUserId;
+    mapping(string => Addresses) private userIdToPublicKey;
 
     constructor() {
-        publicKeyToUserId[0xc2AE2D5523080b64CC788CDDc91FF59a3E29F911] = "tst";
+//        publicKeyToUserId[0xc2AE2D5523080b64CC788CDDc91FF59a3E29F911] = "tst";
     }
 
-    function registerNewUser(string memory userId) public {
-//        require(bytes(PublicKeyToUserId[msg.sender]).length > 0);
-        Console.log(msg.sender);
-        Console.log(userId);
-        userIdToPublicKey[userId] = msg.sender;
+    function registerNewUser() public {
+        //        require(bytes(PublicKeyToUserId[msg.sender]).length > 0);
+//        userIdToPublicKey[userId] = msg.sender;
+//        userIdToPublicKey["tst"] = 0xc2AE2D5523080b64CC788CDDc91FF59a3E29F911;
+        publicKeyToUserId[msg.sender] = userId;
+        userIdToPublicKey[userId].addresses.push(msg.sender);
+    }
+
+    function getPublicAddressesById(string memory userId) public view returns (address[] memory) {
+        return userIdToPublicKey[userId].addresses;
+    }
+
+    function getUserIdByPublicAddress(address publicAddress) public view returns (string memory) {
+        return publicKeyToUserId[publicAddress];
+    }
+
+    function addAddress(string memory userId, address a) private {
+
+//        if (!my_set.is_in[a]) {
+//            my_set.values.push(a);
+//            my_set.is_in[a] = true;
+//        }
+    }
+
+    function createNewId(address sender) external returns (bytes20 blobId) {
+        // Generate the blobId.
+         keccak256(sender);
+        // Make sure this blobId has not been used before.
+        if (blobInfo[blobId].blockNumber != 0) {
+            throw;
+        }
+        // Store blob info in state.
+        blobInfo[blobId] = BlobInfo({
+        flags: bytes4(flagsNonce),
+        revisionCount: 1,
+        blockNumber: uint32(block.number),
+        owner: (bytes4(flagsNonce) & ANONYMOUS != 0) ? 0 : msg.sender,
+        });
+        // Store the first revision in a log in the current block.
+        Store(blobId, 0, contents);
+    }
+
+    struct Addresses {
+        address[] addresses;
+        mapping(uint => bool) isIn;
     }
 }
--- test/who_is.js
+++ test/who_is.js
@@ -9,13 +9,32 @@ contract("WhoIs", function (accounts) {
   let whoIsInstance;
   beforeEach(async () => {
     whoIsInstance = await WhoIs.deployed();
-  })
+  });
+
+  it('should return empty address list when not existed user id given', async () => {
+    const address = await whoIsInstance.getPublicAddressesById.call("unknown");
+    expect(address).to.be.empty;
+  });
+
+  it('should return empty user id if unregistered public address given', async () => {
+    const userId = await whoIsInstance.getUserIdByPublicAddress.call(accounts[0]);
+    expect(userId).to.be.empty;
+  });
+
   describe('Register new user', () => {
-    it('should add new user into userToPublicKey mapping', async () => {
-      let userId = "my-user-id";
-      await whoIsInstance.registerNewUser.call(userId);
-      const address = await whoIsInstance.userIdToPublicKey.call(userId);
-      assert.equal(address, accounts[0]);
+    it('should create new id and map with public key', async () => {
+      // when
+      const userId = await whoIsInstance.registerNewUser({from: accounts[0]});
+
+      // then
+      expect(userId).to.be.not.empty;
+      let userAddresses = await whoIsInstance.getPublicAddressesById(userId);
+      expect(userAddresses).to.contains(accounts[0]);
+      let savedUserId = await whoIsInstance.getUserIdByPublicAddress(accounts[0]);
+      expect(savedUserId).to.be.equal(userId);
+      // console.log(await whoIsInstance.userIdToPublicKey.call("tst"));
+      // const address = await whoIsInstance.userIdToPublicKey.call(userId);
+      // assert.equal(address, accounts[0]);
     });
   });
 });
